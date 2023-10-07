45f0a81ba9aa7d1d0dcc4345066026de52b73534
mustapha amin <mustaphaamin2003@gmail.com>
2023-10-08 03:16:35 +0400

    fixed search icon image path

--- lib/views/screens/home.dart
+++ lib/views/screens/home.dart
@@ -89,7 +89,7 @@ class _HomeState extends State<Home> {
                 ),
                 child: Center(
                   child: Image.asset(
-                    'assets/search.png',
+                    'assets/images/search.png',
                     width: 25,
                   ),
                 ),
